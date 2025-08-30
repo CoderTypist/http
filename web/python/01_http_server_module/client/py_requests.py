c589df5767291d4dedddfeb3ea28b0c1a4cb0f9c
codertypist <cjbargraser@gmail.com>
2025-08-30 15:02:16 -0500

    Made py_requests.py output nicer

--- web/python/01_http_server_module/client/py_requests.py
+++ web/python/01_http_server_module/client/py_requests.py
@@ -4,6 +4,7 @@ import os
 import requests
 import sys
 
+
 def main():
     
     if not (WEB_SERVER_IP := os.environ.get("WEB_SERVER_IP")):
@@ -19,22 +20,20 @@ def main():
         try:
             resp = requests.get(f"http://{WEB_SERVER_IP}:{WEB_SERVER_PORT}/{fname}")
 
-            print("\n-----------------------\n")
-            
-            print("REQUEST:\n")
-            
-            print(f"[{resp.request.url}]")
+            print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n")
+
+            print(f"GET /{fname} HTTP/1.1")
             for header in resp.request.headers:
                 print(f"{header}: {resp.request.headers[header]}")
             
-            print("\n- - - - - - - - - - - -\n")
+            print("\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n")
             
-            print("RESPONSE:\n")
-
+            http_version = f"{resp.raw.version/10}.{resp.raw.version%10}"
+            print(f"HTTP/{http_version} {resp.status_code} {resp.reason}")
             for header in resp.headers:
                 print(f"{header}: {resp.headers[header]}")
-            print(f"[Status Code: {resp.status_code}]")
-            print(resp._content)
+            print('\n\n', end='')
+            print(resp.text, end='')
 
         except requests.exceptions.RequestException as e:
             print(e)
@@ -43,6 +42,6 @@ def main():
     
     print()
 
+
 if __name__ == "__main__":
     main()
-
